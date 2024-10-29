bfab263f0c4edd68c41479f190353cf8468df7e8
VoidedCrystal <Foxco2025@gmail.com>
2024-10-29 10:09:54 -0600

    IT WORKS

--- gofish/discord/js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('login-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-  
-    // Redirect to an informational page
-    window.location.href = '/gofish/phew.html';
-  });
