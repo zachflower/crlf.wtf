c87a8e6ff0d55edad34b29efbd4f57d240d89c8b
leoke599 <leoke073@gmail.com>
2024-10-29 09:47:18 -0600

    Discord

---
+++ gofish/discord/js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('login-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    // Redirect to an informational page
+    window.location.href = '/gofish/phew.html';
+  });
