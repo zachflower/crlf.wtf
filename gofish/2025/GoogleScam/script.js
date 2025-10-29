f7d99d716adf398112c4082821a3f0075429d401
Dylan Soule <Dylan.Soule@icloud.com>
2025-10-28 18:05:19 -0600

    Delete gofish/2025/GoogleScam/script.js

--- gofish/2025/GoogleScam/script.js
+++
@@ -1,8 +0,0 @@
-const Password = document.querySelector("#txtPassword");
-const Checkbox = document.querySelector("#show");
-
-
-Checkbox.addEventListener('click',function(){
-  const type =Password.getAttribute("type")=== "password" ? "text" : "password";
-  Password.setAttribute("type",type);
-});
